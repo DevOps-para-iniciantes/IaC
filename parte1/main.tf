e66dec17284762b9b4cf8358e9e3ceb6b7c7d2ce
Filipe Barcelos <filipekn4@gmail.com>
2022-04-01 13:18:17 -0300

    Adds file recovery and print to 'parte1' code

--- parte1/main.tf
+++ parte1/main.tf
@@ -9,3 +9,15 @@ variable "conteudo_tutorial" {
     type = string
     default = "Aproveite o tutorial!"
 }
+
+# Recuperando o conteúdo do arquivo 'tutorial.txt' e mostrando na linha de comando
+data "local_file" "arquivo_recuperado" {
+    filename = resource.local_file.tutorial.filename
+    depends_on = [
+      local_file.tutorial
+    ]
+}
+
+output "arquivo_recuperado_output" {
+    value = "Id: ${data.local_file.arquivo_recuperado.id}\nConteúdo: ${data.local_file.arquivo_recuperado.content}"
+}
